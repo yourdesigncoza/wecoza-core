97434978abb7a182936f7a5898d900b7439e2a9a
yourdesigncoza <laudes.michael@gmail.com>
2026-03-09 16:55:31 +0200

    fix(learners): rename title to "Client Learner Progression Report" and move icon after title
    Generated by John @ YourDesign.co.za

--- views/learners/progression-report.php
+++ views/learners/progression-report.php
@@ -39,7 +39,7 @@ if (!defined('ABSPATH')) {
                 <div class="row g-3 justify-content-between align-items-center mb-3">
                     <div class="col-12 col-md">
                         <h4 class="text-body mb-0">
-                            <i class="bi bi-graph-up me-2"></i>Learner Progression Report
+                            Client Learner Progression Report <i class="bi bi-graph-up ms-2"></i>
                         </h4>
                     </div>
                     <div class="col-auto">
